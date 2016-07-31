acbb23fe609df6c14b17d59462e6325a2234d186
cnodin <cnodin@gmail.com>
2016-07-31 21:49:57 +0800

    commit ch01、ch02

---
+++ python_crash_course/ch02_variables_and_simple_data_types/birthday.py
@@ -0,0 +1,5 @@
+#coding:utf-8
+age = 23
+#将age由整型转换为字符串
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
---
+++ python_crash_course/ch02_variables_and_simple_data_types/name.py
@@ -0,0 +1,9 @@
+name="ada lovelace"
+#print Ada Lovelace
+print(name.title())
+
+name="Ada Lovelace"
+#print ADA LOVELACE
+print(name.upper())
+#print ada lovelace
+print(name.lower())
