7fc32a8f301de43537a053416c68ff311c509036
cnodin <cnodin@gmail.com>
2016-08-01 10:54:06 +0800

    commit list operations

---
+++ python_crash_course/ch04_working_with_lists/foods.py
@@ -0,0 +1,9 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+firend_foods = my_foods[:]
+fireend_foods = my_foods
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy firend's favorite foods are:")
+print(firend_foods)
---
+++ python_crash_course/ch04_working_with_lists/players.py
@@ -0,0 +1,7 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players)
+print(players[0:3])
+print(players[1:4])
+print(players[:4])
+#print lastest three element
+print(players[-3:])
